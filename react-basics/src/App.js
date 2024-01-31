0c9166af62c4cf9632711d0c66c8107d9419de5f
Anushareact <109461642+Anushareact@users.noreply.github.com>
2024-01-31 16:20:31 +0530

    Creating a custom component part2

--- react-basics/src/App.js
+++ react-basics/src/App.js
@@ -1,11 +1,14 @@
 
 import './App.css';
+import CreatingaCustomComponnetpart2 from './components/CreatingaCustomComponnetpart2';
 
 function App() {
+  const name="anusha"
   return (
     <div className="App">
      {/* way of inject the Hello component to App component */}
-     <Hello/> 
+     <Hello name={name}/> 
+     <CreatingaCustomComponnetpart2/>
     anusha
     </div>
   );
@@ -13,8 +16,8 @@ function App() {
 
 export default App;
 //creating a custom component
-const  Hello=()=>{
-  return <h2>Hello-This is coming from custom component</h2>
+const  Hello=(props)=>{
+  return <h2>Hello{props.name}-This is coming from custom componentppp</h2>;
 
 
 }
---
+++ react-basics/src/components/CreatingaCustomComponnetpart2.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const CreatingaCustomComponnetpart2 = () => {
+  return (
+    <div>CreatingaCustomComponnetpart2</div>
+  )
+}
+
+export default CreatingaCustomComponnetpart2
+// after creating a component we need to import into into app.js file
