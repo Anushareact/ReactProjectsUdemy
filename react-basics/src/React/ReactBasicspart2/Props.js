7b702e45fda12c379a6da8b4dfe3c62ce4f6c17a
Anushareact <109461642+Anushareact@users.noreply.github.com>
2024-02-07 11:39:31 +0530

    props destructuring

--- react-basics/src/React/ReactBasicspart2/Props.js
+++ react-basics/src/React/ReactBasicspart2/Props.js
@@ -1,21 +1,21 @@
 import React from 'react'
-
-const Props = (props) => {
+//props destructuring
+const Props = ({name,role}) => {
   return (
     // <div>Props is a feature in react that allows us to send informations between components</div>
     //Setting default props:there are two types.
     <>
       <div style={{border:"2px grey solid ",padding:"5px",margin:"5px",width:"30%",borderRadius:"10px",display:"inline-block"}}>
         {/* //second type by using or we can set the default props */}
-        <h3>Name:{props.name || "react"}</h3>
-        <h3>Role:{props.role || "react developer"}</h3>
+        <h3>Name:{name }</h3>
+        <h3>Role:{role}</h3>
       </div>
     </>
   );
 }
 //1st type of setting default props
-// Props.defaultProps={
-//     name:"DefaultName",
-//     role:"DeafultRole"
-// }
+Props.defaultProps={
+    name:"DefaultName",
+    role:"DeafultRole"
+}
 export default Props
